027f8bba71593b849ac7bb6366fc7390be934e4a
Jonathan <j_passe@live.fr>
2016-07-12 14:25:09 +0100

    add delete restaurant feature

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -26,6 +26,7 @@ feature 'restaurants' do
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
+      fill_in 'Description', with: 'Finger licking good'
       click_button 'Create Restaurant'
       expect(page).to have_content 'KFC'
       expect(current_path).to eq '/restaurants'
@@ -36,7 +37,7 @@ feature 'restaurants' do
 
     let!(:kfc){ Restaurant.create(name:'KFC')}
 
-    scenario 'allows user to visit a rstaurant page' do
+    scenario 'allows user to visit a restaurant page' do
       visit '/restaurants'
       click_link 'KFC'
       expect(page).to have_content 'KFC'
@@ -44,4 +45,30 @@ feature 'restaurants' do
     end
   end
 
+  context 'editing restaurants' do
+
+    let!(:kfc){ Restaurant.create(name:'KFC', description:'Finger licking good')}
+
+    scenario 'allows user to update restaurant page' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'VFC'
+      fill_in 'Description', with: 'veggie fried chicken is a thing'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'VFC'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'deleting restaurants' do
+
+    let!(:kfc){ Restaurant.create(name:'KFC', description:'Finger licking good')}
+
+    scenario 'allows  user to delete a restaurant' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted'
+    end
+  end
 end
